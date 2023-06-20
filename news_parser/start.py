92c15df5575ddc42853d68263ad70172f553f24e
Semen <semen.trololo@ya.ru>
2023-06-20 22:53:13 +0300

    Update

--- news_parser/start.py
+++ news_parser/start.py
@@ -32,22 +32,24 @@ def status_url(url):
         return 0
 
 
-def add_news(id_news, link, conn, cur):
-    if len(id_news) < 1:
-        return
-    id_news = hashlib.md5(id_news.encode()).hexdigest()
-    id_news = id_news.replace(':', '')
-    sql = "SELECT * FROM rss_news WHERE id_news = '{}';".format(id_news)
-    cur.execute(sql)
-    row = cur.fetchone()
-    if row is None:
-        status = 0
-        flag_send = 0
-        sql = "INSERT INTO rss_news (id_news, link, status, flag_send) VALUES ('{}', '{}', '{}', '{}');".format(
-            id_news, link, status, flag_send)
+def add_news(id_news, link, conn, cur, py_logger):
+    try:
+        if len(id_news) < 1:
+            return
+        id_news = hashlib.md5(id_news.encode()).hexdigest()
+        id_news = id_news.replace(':', '')
+        sql = "SELECT * FROM rss_news WHERE id_news = '{}';".format(id_news)
         cur.execute(sql)
-        conn.commit()
-
+        row = cur.fetchone()
+        if row is None:
+            status = 0
+            flag_send = 0
+            sql = "INSERT INTO rss_news (id_news, link, status, flag_send) VALUES ('{}', '{}', '{}', '{}');".format(
+                id_news, link, status, flag_send)
+            cur.execute(sql)
+            conn.commit()
+    except Exception as e:
+        py_logger.warning(e)
 
 def start(USER_SQL, PASS_SQL, HOST_SQL, PORT_SQL, DB_SQL, py_logger):
 
@@ -60,16 +62,16 @@ def start(USER_SQL, PASS_SQL, HOST_SQL, PORT_SQL, DB_SQL, py_logger):
         _xaker, _xaker_error = parser.get_urls_xakep(py_logger)
         if _pda_error:
             for data in _pda:
-                add_news(data[0], data[1], conn, cur)
+                add_news(data[0], data[1], conn, cur, py_logger)
         if _3dnews_error:
             for data in _3dnews:
-                add_news(data[0], data[1], conn, cur)
+                add_news(data[0], data[1], conn, cur, py_logger)
         if _opennet_error:
             for data in _opennet:
-                add_news(data[0], data[1], conn, cur)
+                add_news(data[0], data[1], conn, cur, py_logger)
         if _xaker_error:
             for data in _xaker:
-                add_news(data[0], data[1], conn, cur)
+                add_news(data[0], data[1], conn, cur, py_logger)
         if _xaker_error and _opennet_error and _pda_error and _3dnews_error:
             break
     py_logger.info('Exit start')
